cc201f6953bef637b0e16011b8343fab9d9e65d9
Richard <richardzehungho@gmail.com>
2023-04-17 15:58:50 -0700

    Add gameFlow to control player turn

--- app.js
+++ app.js
@@ -7,39 +7,74 @@
 //gameboard will store information about the game
 //create private functions to add array content inside the box
 
-const gameboard = (() => {
-  const gameboard = ["", "", "", "", "", "", "", "", ""];
+const gameContainer = document.querySelector("main");
+const gridCell = document.querySelectorAll(".cell");
+
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
 
   const reset = () => {
-    Gameboard.gameboard = ["", "", "", "", "", "", "", "", ""];
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
   };
 
   const marker = (index, value) => {
-    if (
-      gameboard[index] === "" &&
-      gameboard[index] !== "x" &&
-      gameboard[index] !== "o"
-    ) {
-      gameboard[index] = value;
-      return gameboard;
+    if (board[index] === "" && board[index] !== "x" && board[index] !== "o") {
+      board[index] = value;
     }
-    return gameboard;
   };
 
-  const getBoard = () => gameboard;
+  const checkWin = (arr) => {
+    const winningCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  };
 
-  return { gameboard, reset, getBoard, marker };
+  const getBoard = () => board;
+
+  return { board, reset, getBoard, marker, checkWin };
 })();
 
 const render = (() => {
-  const gridCell = document.querySelectorAll(".cell");
-
   const makeGrid = () => {
     gridCell.forEach((index) => {
       gridCell[index.dataset.value].textContent =
-        gameboard.gameboard[index.dataset.value];
+        gameBoard.board[index.dataset.value];
     });
   };
 
   return { makeGrid };
+})();
+
+const gameFlow = (() => {
+  let player1 = "x";
+  let player2 = "o";
+
+  let activePlayer = player2;
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+
+    return activePlayer;
+  };
+
+  return { switchPlayer };
+})();
+
+// const test = gameFlow();
+const screenController = (() => {
+  gridCell.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      gameBoard.marker(e.target.dataset.value, gameFlow.switchPlayer());
+      render.makeGrid();
+    });
+  });
 })();
